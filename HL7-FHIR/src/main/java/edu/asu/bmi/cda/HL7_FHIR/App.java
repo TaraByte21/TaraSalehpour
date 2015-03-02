6393928fc1aae81915c41a6102a1ca8133720688
TaraByte21 <you@example.com>
2015-03-01 22:33:04 -0700

    HW4

---
+++ HL7-FHIR/src/main/java/edu/asu/bmi/cda/HL7_FHIR/App.java
@@ -0,0 +1,13 @@
+package edu.asu.bmi.cda.HL7_FHIR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
